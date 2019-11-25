99d04e7c4446746f2dcc55edfb1cd26368d7cb69
Rayan Avelino <rayanodb@gmail.com>
2019-11-25 11:52:34 -0300

    Cleaning process

--- Java/todoList/src/main/java/projeto/todoList/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package projeto.todoList;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin
-public interface TaskRepository extends CrudRepository<Task, Long> {
-	
-}
